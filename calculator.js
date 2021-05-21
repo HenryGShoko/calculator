7b7b611eec13eb82f5e25f801f644ed67ba78f90
HenryGShoko <gshokx91@gmail.com>
2021-05-21 10:38:00 +0200

    added BMI calculator route

--- calculator.js
+++ calculator.js
@@ -17,6 +17,19 @@ var result = num1 + num2
   res.send("the result of the calculation is " + result)
 })
 
+app.get('/bmicalculator', (req, res) => {
+  res.sendFile(__dirname + '/bmiCalculator.html')
+})
+
+app.post('/bmicalculator', (req, res) => {
+
+  var weight = parseFloat(req.body.weight)
+  var height = parseFloat(req.body.height)
+  var bmi = weight / (height * height)
+  
+    res.send("Your BMI is " + bmi)
+  })
+
 app.listen(3000, () => {
   console.log(`App listening`)
 })
